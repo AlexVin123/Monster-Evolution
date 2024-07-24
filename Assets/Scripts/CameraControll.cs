e780ccbf0ea7c1dcd929ef4128c14db6578068ed
Alex Vin <lexuz1998@gmail.com>
2024-07-24 18:37:42 +0300

    Управление камерой по клику

--- Assets/Scripts/CameraControll.cs
+++ Assets/Scripts/CameraControll.cs
@@ -2,28 +2,51 @@ using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class CameraControll : MonoBehaviour
+public class CameraControll : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
-    [SerializeField] private CinemachineFreeLook _freeLookCam;
+    [SerializeField] private Button _bgBt;
+    [SerializeField] private CinemachineFreeLook _cam;
     private float _Yvalue;
     private float _Xvalue;
 
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _cam.m_XAxis.m_MaxSpeed = _Xvalue;
+        _cam.m_YAxis.m_MaxSpeed = _Yvalue;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+
+        _cam.m_XAxis.m_MaxSpeed = 0;
+        _cam.m_YAxis.m_MaxSpeed = 0;
+    }
+
     private void Start()
     {
-        _Yvalue = _freeLookCam.m_YAxis.Value;
-        _Xvalue = _freeLookCam.m_XAxis.Value;
+        _Yvalue = _cam.m_YAxis.m_MaxSpeed;
+        _Xvalue = _cam.m_XAxis.m_MaxSpeed;
+        _cam.m_XAxis.m_MaxSpeed = 0;
+        _cam.m_YAxis.m_MaxSpeed = 0;
     }
 
-    public void FrezeCam()
+    private void Update()
     {
-        _freeLookCam.m_YAxis.Value = 0;
-        _freeLookCam.m_XAxis.Value = 0;
+        if(Input.GetMouseButtonDown(1))
+        {
+            _cam.m_XAxis.m_MaxSpeed = _Yvalue;
+            _cam.m_YAxis.m_MaxSpeed = _Xvalue;
+        }
+
+        if(Input.GetMouseButtonUp(1)) 
+        {
+
+            _cam.m_XAxis.m_MaxSpeed = 0;
+            _cam.m_YAxis.m_MaxSpeed = 0;
+        }
     }
 
-    public void UnFrezeCam()
-    {
-        _freeLookCam.m_YAxis.Value = _Yvalue;
-        _freeLookCam.m_XAxis.Value = _Xvalue;
-    }
 }
